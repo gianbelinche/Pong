bd18a1acafbe2fe2c3c5c46a14233f09b9fb4c9e
FrancoBar <44034749+FrancoBar@users.noreply.github.com>
2019-11-08 20:04:44 -0300

    Aclaro que hace cada cosa y agrego boton

--- pong.ino
+++ pong.ino
@@ -1,20 +1,34 @@
 #include "LedControl.h"
 
+//Actualizar los pines una vez montemos el dispositivo
 LedControl lcl=LedControl(12,11,10,1);
 
-
+//
+int pinDelBoton = 8;
+int estadoBoton = 0;
 
 void setup() {
-  // put your setup code here, to run once:
+
+  //Apaga todos los LEDS
   for(int index=0 ; index < lcl.getDeviceCount() ; index++) {
       lcl.shutdown(index,false);
 
+  //Inicializa variable de control de matriz
   LedControl.clearDisplay(0) // 0 para la primer matriz, uno para la segunda  
   lcl.setLed(0,2,7,true); //prende en led (2,7), 0 es la matriz a prender.
+
+  //Inicializa boton
+  pinMode(pinDelBoton, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  estadoBoton = digitalRead(pinDelBoton);
+  
+  if( estadoBoton == 1) {
+
+  } else {
+
+  }
 
 }
 
