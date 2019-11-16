0553e530521ffca560b9aa1c94e40a657cb11fc4
FrancoBar <44034749+FrancoBar@users.noreply.github.com>
2019-11-15 22:48:12 -0300

    Agrego gestion de puntaje

--- pong.ino
+++ pong.ino
@@ -30,7 +30,7 @@
 
 typedef struct paleta{
 
-  char x;
+  int x;
   int y;
 
 }paleta_t;
@@ -39,10 +39,10 @@ typedef struct paleta{
 
 typedef struct pelota{
 
-  char x;
-  char y;
-  char vel_x;
-  char vel_y;
+  int x;
+  int y;
+  int vel_x;
+  int vel_y;
 
 }pelota_t;
 
@@ -55,17 +55,15 @@ typedef struct pelota{
 
 LedControl lcl=LedControl(PIN_MATRIZ_DATA_IN,PIN_MATRIZ_CLK,PIN_MATRIZ_LOAD,2);  //Maneja las matrices de leds
 
-paleta_t paleta_izq = {1,2};  //Inicializacion de las paletas y la pelota
-
+paleta_t paleta_izq = {1,2};
 paleta_t paleta_der = {14,2};
-
 pelota_t pelota = {7,rand() % 8,-1,1} ;
 
 int vel_paleta_der = 0;
-
 int vel_paleta_izq = 0;
 
-
+int puntaje_izq = 0;
+int puntaje_der = 0;
 
 
 /******************************
@@ -80,7 +78,6 @@ int vel_paleta_izq = 0;
 /* * * * * * * * * * * * * * * * *
  *  FUNCIONES PRINCIPALES
  */
-
 
 
  
@@ -123,6 +120,8 @@ void loop() {
  moverPaleta(&paleta_izq,vel_paleta_izq);
 
  moverPaleta(&paleta_der,vel_paleta_der);
+
+ moverPelota();
 
  gestionarCondicionesDeVictoria();
 
@@ -189,11 +188,8 @@ void dibujar(int x, int y, int estado){
 
 
 void administrarEntrada(){
-
   vel_paleta_der =  !digitalRead(PIN_BOTON_DER_ARRIBA) - !digitalRead(PIN_BOTON_DER_ABAJO);
   vel_paleta_izq =  !digitalRead(PIN_BOTON_IZQ_ARRIBA) - !digitalRead(PIN_BOTON_IZQ_ABAJO);
-  moverPelota();
-
 }
 
 
@@ -247,9 +243,50 @@ void reiniciar(){
 
   transicion();
 
+  lcl.clearDisplay(PRIMERA_MATRIZ);
+  lcl.clearDisplay(SEGUNDA_MATRIZ);
+
   inicializar();
+  dibujarPuntaje(PRIMERA_MATRIZ,puntaje_izq);
+  dibujarPuntaje(SEGUNDA_MATRIZ,puntaje_der);
+  delay(1000);
+}
+
+void dibujarPuntaje(int matriz,int puntaje){
+  if(puntaje == 0) return;
+  const int x_pantalla = (matriz)? 1 : 9;
+
+  for(int i = 1;i <= POSICION_FILA_MAXIMA - 1;i++){
+    dibujar(x_pantalla,i,1);
+  }
+  
+  if(puntaje == 1) return;
+
+  for(int i = x_pantalla;i <= POSICION_FILA_MAXIMA - 1;i++){
+    dibujar(i,1,1);
+  }
+   
+  if(puntaje == 2) return;
+
+  for(int i = 1;i <= POSICION_FILA_MAXIMA - 1;i++){
+    dibujar(x_pantalla + 4,i,1);
+  }
+
+  if(puntaje == 3) return;
+
+  for(int i = x_pantalla;i <= POSICION_FILA_MAXIMA - 1;i++){
+    dibujar(i,6,1);
+  }
+  
+  if(puntaje == 4) return;
+
+  for(int i = 5;i > 1;i--){
+    dibujar(x_pantalla + 1 + 5 - i,i,1);
+    dibujar(x_pantalla + 2 + 5 - i,i,1);
+  }
 
 }
+
 
 void dibujarT(int x,int y,int estado){
   dibujar(x,y,estado);
@@ -416,12 +453,6 @@ void moverPelota(){
 
   actualizarPelota(x_anterior,y_anterior);
 
-  if (pelota.x == 0 or pelota.x == 15){
-
-    reiniciar();
-
-  }
-
 }
 
 
@@ -429,7 +460,21 @@ void moverPelota(){
 
 void gestionarCondicionesDeVictoria(){
 
+  if (pelota.x == 0 or pelota.x == 15){
 
+    if(puntaje_der == 5 || puntaje_izq == 5){
+      puntaje_der = 0;
+      puntaje_izq = 0;
+     }
+     
+    if(pelota.x == 0)
+      puntaje_der++;
+    else
+      puntaje_izq++;
+      
+    reiniciar();
+  }
+  
 
 }
 
